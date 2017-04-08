ec85a6d8ee4fcce24d029fc353a31ce02bab498c
aline.poliveira3 <aline.poliveira3@CAS555557W0704.senacsp.edu.br>
2017-04-07 22:26:00 -0300

    exercicio 5

--- src/lista03/ClassEs_05.java
+++ src/lista03/ClassEs_05.java
@@ -16,19 +16,14 @@ public class ClassEs_05 {
     public static void main(String[] args) {
         Scanner NN = new Scanner(System.in);
 
-        for (int i = 0; i < 2; i++) {
-            System.out.print("Digite um número entre 2 e 1000: ");
-            int N = NN.nextInt();
-            int j = 1;
-            if (N > 2 && N < 1000) {
-                System.out.println("número invalido");
-            } else {
-                do {
-                    System.out.println(j + " x " + N + " = " + (j * N));
-                    j++;
-                } while (j <= 10);
-            }
+        System.out.print("Digite um número entre 2 e 1000: ");
+        int N = NN.nextInt();
+        int j = 1;
 
-        }
+        do {
+            System.out.println(j + " x " + N + " = " + (j * N));
+            j++;
+        } while (j <= 10);
+
     }
 }
